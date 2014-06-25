f5ce2bf410d875e402ef5c8c4fa785cbbf3f65af
Matt Riddle <mriddle89@gmail.com>
2014-06-25 15:57:45 -0500

    Abort will exit an exitcode, without a stack trace

--- lib/bundle_cache/validation.rb
+++ lib/bundle_cache/validation.rb
@@ -5,10 +5,7 @@ module BundleCache
     required_env = %w(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_S3_BUCKET BUNDLE_ARCHIVE)
 
     required_env.each do |var|
-      unless ENV[var]
-        puts "Missing ENV[#{var}]"
-        exit 1
-      end
+      abort("Missing ENV[#{var}]") unless ENV[var]
     end
   end
 
