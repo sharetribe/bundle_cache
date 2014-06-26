f42cbd96600e1b5ad1f5a62a448f5bc20af84a1f
Matt Riddle <mriddle89@gmail.com>
2014-06-26 09:51:28 -0500

    Add support for use of aws_session_token
    BundleCache would previously abort when using session_token instead of access_key combination

--- lib/bundle_cache/validation.rb
+++ lib/bundle_cache/validation.rb
@@ -2,11 +2,19 @@ module BundleCache
   extend self
 
   def verify_env
-    required_env = %w(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_S3_BUCKET BUNDLE_ARCHIVE)
-
-    required_env.each do |var|
+    %w(AWS_S3_BUCKET BUNDLE_ARCHIVE).each do |var|
       abort("Missing ENV[#{var}]") unless ENV[var]
     end
+
+    abort("Missing required AWS credentials") if (session_token_missing? && access_keys_missing?)
+  end
+
+  def session_token_missing?
+    ENV['AWS_SESSION_TOKEN'].nil?
+  end
+
+  def access_keys_missing?
+    ENV['AWS_ACCESS_KEY_ID'].nil? || ENV['AWS_SECRET_ACCESS_KEY'].nil?
   end
 
 end
