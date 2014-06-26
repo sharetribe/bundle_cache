60fcad6e2bc9c497bdcf2e0550098ec74d5aef11
Matt Riddle <mriddle89@gmail.com>
2014-06-26 10:27:11 -0500

    Add deprecation warning
    Avoid breaking change, notify user of deprecation and set correct environment variables

--- lib/bundle_cache/validation.rb
+++ lib/bundle_cache/validation.rb
@@ -2,6 +2,8 @@ module BundleCache
   extend self
 
   def verify_env
+    deprecation_warning
+
     %w(AWS_S3_BUCKET BUNDLE_ARCHIVE).each do |var|
       abort("Missing ENV[#{var}]") unless ENV[var]
     end
@@ -15,6 +17,26 @@ module BundleCache
 
   def access_keys_missing?
     ENV['AWS_ACCESS_KEY_ID'].nil? || ENV['AWS_SECRET_ACCESS_KEY'].nil?
+  end
+
+  def deprecation_warning
+    if !!ENV['AWS_S3_KEY']
+      puts "The 'AWS_S3_KEY' environment variable is deprecated and will be removed in the future."
+      puts "Please set 'AWS_ACCESS_KEY_ID' instead"
+      ENV['AWS_ACCESS_KEY_ID'] = ENV['AWS_S3_KEY']
+    end
+
+    if !!ENV['AWS_S3_SECRET']
+      puts "The 'AWS_S3_SECRET' environment variable is deprecated and will be removed in the future."
+      puts "Please set 'AWS_SECRET_ACCESS_KEY' instead"
+      ENV['AWS_SECRET_ACCESS_KEY'] = ENV['AWS_S3_SECRET']
+    end
+
+    if !!ENV['AWS_S3_REGION']
+      puts "The 'AWS_S3_REGION' environment variable is deprecated and will be removed in the future."
+      puts "Please set 'AWS_DEFAULT_REGION' instead"
+      ENV['AWS_DEFAULT_REGION'] = ENV['AWS_S3_REGION']
+    end
   end
 
 end
